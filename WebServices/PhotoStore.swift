124d0dee029536d9c814649130eec9ea63e0109e
Tianbo <tianbo@hey.com>
2022-12-29 15:31:08 -0800

    fetching an displaying image

--- WebServices/PhotoStore.swift
+++ WebServices/PhotoStore.swift
@@ -5,7 +5,12 @@
 //  Created by Tianbo Qiu on 12/29/22.
 //
 
-import Foundation
+import UIKit
+
+enum PhotoError: Error {
+    case imageCreationError
+    case missingImageURL
+}
 
 class PhotoStore {
     private let session: URLSession = {
@@ -32,7 +37,9 @@ class PhotoStore {
 //            }
             
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
@@ -43,5 +50,34 @@ class PhotoStore {
         }
         
         return FlickerAPI.photos(fromJSON: jsonData)
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (Result<UIImage, Error>) -> Void) {
+        guard let photoURL = photo.remoteURL, let url = URL(string: photoURL) else {
+            completion(.failure(PhotoError.missingImageURL))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> Result<UIImage, Error> {
+        guard let imageData = data, let image = UIImage(data: imageData) else {
+            if data == nil {
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+        
+        return .success(image)
     }
 }
